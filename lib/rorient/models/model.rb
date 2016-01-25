835920fbc3088e9cdf53bcd95472e67804cfbe41
Tommaso Patrizi <tommasop@gmail.com>
2016-01-25 20:55:24 +0100

    query builder

--- lib/rorient/models/model.rb
+++ lib/rorient/models/model.rb
@@ -176,6 +176,41 @@ module Rorient
       orientdb.document.exists(rid: rid)
     end
 
+    # An Ohm::Set wrapper for Model.key[:all].
+    def self.all
+      Ohm::Set.new(self, key, key[:all])
+    end
+
+    # Syntactic sugar for Model.new(atts).save
+    def self.create(atts = {})
+      new(atts).save
+    end
+    
+    def self.query_builder(params:)
+      query = ["from #{self.name} where"]
+      # if I only have the order param strip off where
+      query[0].gsub!(" where", "") if params.keys.count == 1 && params["order"]
+      # if these three params are present we have a spatial query
+      if ["my_lat", "my_long", "zoom"].all?{|spatial_param| params.key? spatial_param}
+        distance_in_km = zoom_to_distance_in_km(params["zoom"])
+        query.insert(0, "*,$distance") 
+        query << "[wgs84_lat,wgs84_long,$spatial] NEAR [#{params["my_lat"]},#{params["my_long"]},{'maxDistance':#{distance_in_km}}]"
+      end
+      # retrieve all object properties which are the only valid parameters
+      attributes = orientdb.query.execute(query_text: URI.encode("SELECT properties FROM ( SELECT expand( classes ) FROM metadata:schema ) WHERE name = '#{self.name}'"))[:properties].map{ |prop| prop[:name]}
+      # array intersection only keys that are properties will be left
+      query_attributes = (attributes & params.keys)
+      # if there is also a spatial query we need to add an AND
+      query.insert(3, "AND") if query.length == 3 && query_attributes.present?
+      # each property becomes a where property= ecc.
+      query_attributes.each_with_index do |attr,i|
+        query << "#{attr} = '#{params[attr]}' #{"AND" if i < query_attributes.length - 1}" 
+      end
+      # adding an order by clause
+      query << "order by #{params["order"]}" if params["order"]
+      orientdb.query.execute(query_text: URI.encode(query.join(" ")))
+    end
+
     def initialize(atts={})
       @attributes = {}
       @_memo = {}
