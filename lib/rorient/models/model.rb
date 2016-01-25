6dfaa09a31a2fc3f098cdff5a71f872cb30c512e
Tommaso Patrizi <tommasop@gmail.com>
2016-01-25 21:02:31 +0100

    result

--- lib/rorient/models/model.rb
+++ lib/rorient/models/model.rb
@@ -197,7 +197,7 @@ module Rorient
         query << "[wgs84_lat,wgs84_long,$spatial] NEAR [#{params["my_lat"]},#{params["my_long"]},{'maxDistance':#{distance_in_km}}]"
       end
       # retrieve all object properties which are the only valid parameters
-      attributes = orientdb.query.execute(query_text: URI.encode("SELECT properties FROM ( SELECT expand( classes ) FROM metadata:schema ) WHERE name = '#{self.name}'"))["properties"].map{ |prop| prop["name"]}
+      attributes = orientdb.query.execute(query_text: URI.encode("SELECT properties FROM ( SELECT expand( classes ) FROM metadata:schema ) WHERE name = '#{self.name}'"))["result"]["properties"].map{ |prop| prop["name"]}
       # array intersection only keys that are properties will be left
       query_attributes = (attributes & params.keys)
       # if there is also a spatial query we need to add an AND
