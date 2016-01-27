d49114277ac34786fb5402239d6dddcc4bacdb51
Tommaso Patrizi <tommasop@gmail.com>
2016-01-27 14:54:14 +0100

    cccc

--- lib/rorient/models/rid.rb
+++ lib/rorient/models/rid.rb
@@ -2,6 +2,8 @@ module Rorient
   class Rid
     def self.get(atts={})
       puts atts
+      puts atts.keys
+      puts atts.keys.include?(:rid)
       case 
       when atts.keys.include?(:@rid)
         return atts[:@rid].gsub!("#",'')
