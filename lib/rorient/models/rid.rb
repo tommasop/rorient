c0f5d3c16547116e88a5f9de6f92db6088697acc
Tommaso Patrizi <tommasop@gmail.com>
2016-01-27 15:03:04 +0100

    fix

--- lib/rorient/models/rid.rb
+++ lib/rorient/models/rid.rb
@@ -1,17 +1,15 @@
 module Rorient
   class Rid
     def self.get(atts={})
-      puts atts
-      puts atts.keys
-      puts atts.keys.include?(:rid)
       case 
       when atts.keys.include?(:@rid)
-        return atts[:@rid].gsub!("#",'')
+        puts ":@rid"
+        atts[:@rid].gsub("#",'')
       when atts.keys.include?(:rid)
-        puts atts[:rid]
-        return atts[:rid].gsub!("#",'')
-      default
-        return nil
+        puts ":rid"
+        atts[:rid].gsub("#",'')
+      else
+        nil
       end
     end
   end
