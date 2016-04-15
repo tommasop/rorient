354aae6925deebd61caf51dc487f095458148e93
Tommaso Patrizi <tommasop@gmail.com>
2016-04-15 17:24:31 +0200

    to respect test order

--- test/helper.rb
+++ test/helper.rb
@@ -10,7 +10,11 @@ require 'minitest/autorun'
 require "rack-minitest/test"
 require 'rorient'
 
-class Rorient::TestCase < Minitest::Test
+class Rorient::TestCase < Minitest::Unit::TestCase
+  def test_order
+    :alpha
+  end
+
   def setup
     @db = Rorient.connect(server: "http://159.122.132.173:2480", user: 'test', password: 'test_PWD$2016', db_name: 'test_rorient') 
   end
--- test/resources/test_batch_resource.rb
+++ test/resources/test_batch_resource.rb
@@ -1,8 +1,7 @@
 require File.expand_path('../../helper', __FILE__)
 
 class TestBatchResource < Rorient::TestCase
-  def test_statements_execution
-    sql_statements = { transaction: false,
+  UP_STATEMENTS = { transaction: false,
                        operations: [
                          {
                            type: "script",
@@ -18,7 +17,25 @@ class TestBatchResource < Rorient::TestCase
                          }
                        ]
                      }
-    assert_equal({:result=>[{:@type=>"d", :@version=>0, :value=>0, :@fieldTypes=>"value=l"}]}, @db.batch.execute(sql_statements))
+
+    DOWN_STATEMENTS = { transaction: false,
+                       operations: [
+                         {
+                           type: "script",
+                           language: "sql",
+                           script: [ 
+                                     "DROP CLASS RorientTest"
+                           ]
+                         }
+                       ]
+                     }
+
+  def test_statements_execution
+    assert_equal({:result=>[{:@type=>"d", :@version=>0, :value=>0, :@fieldTypes=>"value=l"}]}, @db.batch.execute(UP_STATEMENTS))
+  end
+
+  def test_statement_failure_on_repetition
+    assert_includes(@db.batch.execute(UP_STATEMENTS), ":code=>500")
   end
 end
 
