3d486b56ea8e6b25f7a2093cf7d33f9d054db778
Tommaso Patrizi <tpatrizi@makeplan.it>
2017-03-15 11:00:07 +0100

    traverse wired lacking while

--- lib/rorient/queries/maker/traverse.rb
+++ lib/rorient/queries/maker/traverse.rb
@@ -1,17 +1,22 @@
 class Rorient::Queries::Maker::Traverse
   include Enumerable
   include Rorient::Queries::Maker::Util
+  
+  class SubqueryAlreadyInitialized < StandardError; end
+  class FromAlreadyInitialized < StandardError; end
+  class MaxdepthAlreadyInitialized < StandardError; end
+  class LimitAlreadyInitialized < StandardError; end
 
-  attr_reader :_fields, :_from, :_maxdepth, :_while, :_limit, :_strategy
+  attr_reader :_fields, :_maxdepth, :_while, :_limit, :_strategy
 
   def initialize
     @query = ["TRAVERSE"]
     @_fields = []
-    @_from = []
+    @_from = ["FROM"]
     @_maxdepth = nil
     @_while = {}
     @_limit = nil
-    @_strategy = nil
+    @_strategy = nil 
   end
 
   def fields(*args)
@@ -19,18 +24,45 @@ class Rorient::Queries::Maker::Traverse
     self
   end
 
+  def _fields
+    @_fields.join(",")
+  end
+
   def from(*args)
-    @_from = @_from + parse_args(args)
+    @_from << parse_args(args)
     self
   end
 
+  def _from
+    @subquery || @_from
+  end
+
+  def maxdepth(depth_level = 0)
+    raise LimitAlreadyInitialized if @_limit
+    @_maxdepth = "MAXDEPTH #{depth_level}"
+    self
+  end
+  
+  def limit(record_number = 20)
+    raise MaxdepthAlreadyInitialized if @_maxdepth
+    @_limit = "LIMIT #{record_number}"
+    self
+  end
+
+  def strategy(type = "DEPTH_FIRST")
+    @_strategy = "STRATEGY #{type}"
+    self
+  end
+  
   def subquery(type: "Select")
     raise SubqueryAlreadyInitialized if @subquery
-    raise FromAlreadyInitialized if !@from.empty?
+    raise FromAlreadyInitialized if !@_from.empty?
     @subquery ||= "Rorient::Queries::Maker::#{type}".constantize.new
   end
 
   def query
+    @query << _fields << _from << _maxdepth << _while << _limit << _strategy
+    @query.compact.flatten.join(" ")
   end
 end
 
