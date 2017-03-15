c0eec59e9267896b318c9d4736c80e42d225149b
Tommaso Patrizi <tpatrizi@makeplan.it>
2017-03-15 11:16:04 +0100

    select minimal

--- lib/rorient/queries/maker/select.rb
+++ lib/rorient/queries/maker/select.rb
@@ -1,29 +1,61 @@
 class Rorient::Queries::Maker::Select
   include Enumerable
   include Rorient::Queries::Maker::Util
+  
+  class SubqueryAlreadyInitialized < StandardError; end
+  class FromAlreadyInitialized < StandardError; end
+  class WrongOrderDir < StandardError; end
+  class LimitAlreadyInitialized < StandardError; end
+
+  attr_reader :_fields, :_where, :_limit, :_order
 
   def initialize
     @query = ["SELECT"]
-    @criteria = {fields: [], from: [], where: {}, limit: nil, order: nil}
+    @_fields = []
+    @_from = ["FROM"]
+    @_where = {}
+    @_limit = nil
+    @_order = nil 
   end
 
   def fields(*args)
-    criteria[:fields] = criteria[:fields] + parse_fields(args)
+    @_fields = @_fields + parse_args(args)
     self
+  end
+
+  def _fields
+    @_fields.join(",")
   end
 
   def from(*args)
-    criteria[:from] = criteria[:from] + parse_from(args.count == 1 ? args[0] : args)
+    @_from << parse_args(args)
     self
   end
 
-  def nested(arg)
-    raise WrongNestedQuery unless [Rorient::Queries::Maker::Select, Rorient::Queries::Maker::Traverse].include?(arg.class)
-    from(arg.query)
+  def _from
+    @subquery || @_from
   end
 
-  def maxdepth(arg)
-    raise WrongA
+  def limit(record_number = 20)
+    @_limit = "LIMIT #{record_number}"
+    self
   end
 
+  def order(dir = "ASC", *args)
+    raise WrongOrderDir if ! ["ASC", "DESC"].include?(dir)
+    @_order = "ORDER BY #{parse_args(args).join(",")} #{dir}"
+    self
+  end
+  
+  def subquery(type: "Select")
+    raise SubqueryAlreadyInitialized if @subquery
+    raise FromAlreadyInitialized if !@_from.empty?
+    @subquery ||= "Rorient::Queries::Maker::#{type}".constantize.new
+  end
+
+  def query
+    @query << _fields << _from << _while << _limit << _order
+    @query.compact.flatten.join(" ")
+  end
 end
+
