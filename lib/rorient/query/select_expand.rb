88cd071f2ad75898a3b07860166d4c402547ee16
Tommaso Patrizi <tpatrizi@makeplan.it>
2017-03-21 14:32:44 +0100

    refactored as fields interfaces

--- lib/rorient/query/select_expand.rb
+++ lib/rorient/query/select_expand.rb
@@ -19,31 +19,28 @@ class Rorient::Query::SelectExpand
   end
 
   # I need to know:
-  # 1. v or e default "" which means in()
-  # 2. an edge || vertex class 
-  # 3. a named hash of filters achieved with the ruby 2 double splat [**] operator
-  def in(v_or_e = "", type = nil, **args)
-    field = "in#{v_or_e}"
+  # 1. direction: in | out | both
+  # 2. v or e default "" which means in()
+  # 3. an edge || vertex class 
+  # 4. a named hash of filters achieved with the ruby 2 double splat [**] operator
+  def fields(direction = nil, v_or_e = "", type = nil, **args)
+    field = "#{direction}#{v_or_e}"
     type ? field << "('#{type}')" : field << "()"
-    field << "[#{args.map{|k,v| "#{k}=#{v}"}.join(",")}]" if !args.empty?
-    @_fields << field 
-    self
-  end
-  
-  def out(v_or_e = "", type = nil, **args)
-    field = "out#{v_or_e}"
-    type ? field << "('#{type}')" : field << "()"
-    field << "[#{args.map{|k,v| "#{k}=#{v}"}.join(",")}]" if !args.empty?
+    field << "#{args.map{|k,v| "[#{k}=#{v}]"}.join(",")}" if !args.empty?
     @_fields << field 
     self
   end
 
+  def in(v_or_e = "", type = nil, **args)
+    fields(:in, v_or_e, type, args)
+  end
+  
+  def out(v_or_e = "", type = nil, **args)
+    fields(:out, v_or_e, type, args)
+  end
+
   def both(v_or_e = "", type = nil, **args)
-    field = "both#{v_or_e}"
-    type ? field << "('#{type}')" : field << "()"
-    field << "[#{args.map{|k,v| "#{k}=#{v}"}.join(",")}]" if !args.empty?
-    @_fields << field 
-    self
+    fields(:both, v_or_e, type, args)
   end
 
   def from(args)
